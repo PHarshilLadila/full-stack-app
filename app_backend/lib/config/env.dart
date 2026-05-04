782482427580d0283802a4a0bbd0a4cda8fcedf5
Harshil Gajipara <harshilgajipara.ladila@gmail.com>
2026-05-04 10:05:27 +0530

    added docker support

--- app_backend/lib/config/env.dart
+++ app_backend/lib/config/env.dart
@@ -1,10 +1,20 @@
-// ignore_for_file: public_member_api_docs
+// // ignore_for_file: public_member_api_docs
 
-import 'package:dotenv/dotenv.dart';
+// import 'package:dotenv/dotenv.dart';
+
+// class Env {
+//   static final env = DotEnv()..load();
+
+//   static String get mongoUrl => env['MONGO_URL'] ?? '';
+//   static String get jwtSecret => env['JWT_SECRET'] ?? '';
+// }
+
+import 'dart:io';
 
 class Env {
-  static final env = DotEnv()..load();
-
-  static String get mongoUrl => env['MONGO_URL'] ?? '';
-  static String get jwtSecret => env['JWT_SECRET'] ?? '';
+  static String get mongoUrl => Platform.environment['MONGO_URL'] ?? '';
+  static String get jwtSecret => Platform.environment['JWT_SECRET'] ?? '';
 }
+
+// MONGO_URL=mongodb+srv://harshilgajiparaladila_db_user:Gajipara%4096@usercluster.4cpa4pd.mongodb.net/mydb?retryWrites=true&w=majority
+// JWT_SECRET=Qvvt5vAUuNmdaebL3sakv15rSqu5XJpYOuMobvqwGbs=
