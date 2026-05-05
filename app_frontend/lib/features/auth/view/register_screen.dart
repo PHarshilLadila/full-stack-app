6f14f1b02cc0409de627b3cdde0e8a73d8b86736
Harshil Gajipara <harshilgajipara.ladila@gmail.com>
2026-05-05 12:06:45 +0530

    Added new file for custom form field.

--- app_frontend/lib/features/auth/view/register_screen.dart
+++ app_frontend/lib/features/auth/view/register_screen.dart
@@ -2,6 +2,7 @@ import 'package:app_frontend/features/auth/bloc/auth_bloc.dart';
 import 'package:app_frontend/features/auth/bloc/auth_event.dart';
 import 'package:app_frontend/features/auth/bloc/auth_state.dart';
 import 'package:app_frontend/features/auth/model/register_model.dart';
+import 'package:app_frontend/utils/common/custom_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -321,78 +322,5 @@ class _RegisterFormState extends State<RegisterForm> {
     passwordController.dispose();
     confirmPasswordController.dispose();
     super.dispose();
-  }
-}
-
-class AppTextField extends StatelessWidget {
-  final TextEditingController controller;
-  final String hintText;
-  final IconData icon;
-  final String? Function(String?)? validator;
-  final bool obscureText;
-  final TextInputType keyboardType;
-
-  const AppTextField({
-    super.key,
-    required this.controller,
-    required this.hintText,
-    required this.icon,
-    this.validator,
-    this.obscureText = false,
-    this.keyboardType = TextInputType.text,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      cursorColor: Colors.amber,
-      obscureText: obscureText,
-      keyboardType: keyboardType,
-      style: const TextStyle(color: Colors.black),
-
-      decoration: InputDecoration(
-        hintText: hintText,
-        hintStyle: const TextStyle(color: Colors.grey),
-        filled: true,
-        fillColor: Colors.white,
-
-        prefixIcon: Padding(
-          padding: const EdgeInsets.only(left: 16.0),
-          child: Icon(icon, color: Colors.grey),
-        ),
-
-        contentPadding: const EdgeInsets.symmetric(
-          horizontal: 12,
-          vertical: 14,
-        ),
-
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(100),
-          borderSide: BorderSide(
-            color: Colors.grey.withOpacity(0.6),
-            width: 0.5,
-          ),
-        ),
-
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(100),
-          borderSide: BorderSide(
-            color: Colors.grey.withOpacity(0.6),
-            width: 0.5,
-          ),
-        ),
-
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(100),
-          borderSide: BorderSide(
-            color: Colors.grey.withOpacity(0.6),
-            width: 0.5,
-          ),
-        ),
-      ),
-
-      validator: validator,
-    );
   }
 }
