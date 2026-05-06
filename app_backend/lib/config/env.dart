365d69d8375ebb5be099f82f0e9916ff9ca23d97
Harshil Gajipara <harshilgajipara.ladila@gmail.com>
2026-05-06 09:24:27 +0530

    Update env.dart

--- app_backend/lib/config/env.dart
+++ app_backend/lib/config/env.dart
@@ -1,25 +1,3 @@
-// // // ignore_for_file: public_member_api_docs
-
-// // import 'package:dotenv/dotenv.dart';
-
-// // class Env {
-// //   static final env = DotEnv()..load();
-
-// //   static String get mongoUrl => env['MONGO_URL'] ?? '';
-// //   static String get jwtSecret => env['JWT_SECRET'] ?? '';
-// // }
-
-// import 'dart:io';
-
-// class Env {
-//   static String get mongoUrl => Platform.environment['MONGO_URL'] ?? '';
-//   static String get jwtSecret => Platform.environment['JWT_SECRET'] ?? '';
-// }
-
-// // MONGO_URL=mongodb+srv://harshilgajiparaladila_db_user:Gajipara%4096@usercluster.4cpa4pd.mongodb.net/mydb?retryWrites=true&w=majority
-// //           mongodb+srv://harshilgajiparaladila_db_user:Gajipara%4096@usercluster.4cpa4pd.mongodb.net/mydb?retryWrites=true&w=majority&tls=true
-// // JWT_SECRET=Qvvt5vAUuNmdaebL3sakv15rSqu5XJpYOuMobvqwGbs=
-
 // ignore_for_file: public_member_api_docs
 
 import 'package:dotenv/dotenv.dart';
