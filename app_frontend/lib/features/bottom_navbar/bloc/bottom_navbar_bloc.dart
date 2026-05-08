a7400fa37046c352ee6c9dabceeb20e0a2038fad
Harshil Gajipara <harshilgajipara.ladila@gmail.com>
2026-05-08 10:02:48 +0530

    created bottom navigation bar and file & folder structure with home, categories, cart and profile for the customer.

---
+++ app_frontend/lib/features/bottom_navbar/bloc/bottom_navbar_bloc.dart
@@ -0,0 +1,12 @@
+import 'package:app_frontend/features/bottom_navbar/bloc/bottom_navbar_event.dart';
+import 'package:app_frontend/features/bottom_navbar/bloc/bottom_navbar_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class BottomNavigationBloc
+    extends Bloc<BottomNavigationEvent, BottomNavigationState> {
+  BottomNavigationBloc() : super(BottomNavigationInitial()) {
+    on<BottomNavigationItemTapped>((event, emit) {
+      emit(BottomNavigationUpdated(event.index));
+    });
+  }
+}
