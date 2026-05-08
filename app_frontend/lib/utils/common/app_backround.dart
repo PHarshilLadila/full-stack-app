1d4164e72d56b56f4181247a5d92e3218ece77d2
Harshil Gajipara <harshilgajipara.ladila@gmail.com>
2026-05-08 10:29:58 +0530

    Created custom app background file for use in all screens.

---
+++ app_frontend/lib/utils/common/app_backround.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+ 
+class AppBackround extends StatelessWidget {
+  const AppBackround({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Positioned(
+          top: 0,
+          left: 0,
+          child: Container(
+            width: 200,
+            height: 180,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.transparent, Colors.transparent],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: BorderRadius.only(bottomRight: Radius.circular(60)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.yellow.withOpacity(0.09),
+                  blurRadius: 80,
+                  spreadRadius: 80,
+                  offset: Offset(0, 0),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.centerRight,
+          child: Container(
+            width: 200,
+            height: 180,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.transparent, Colors.transparent],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: BorderRadius.circular(100),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.blue.withOpacity(0.07),
+                  blurRadius: 50,
+                  spreadRadius: 40,
+                  offset: Offset(0, 0),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          right: 0,
+          bottom: 0,
+          child: Container(
+            width: 200,
+            height: 180,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.transparent, Colors.transparent],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: BorderRadius.circular(100),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.pink.withOpacity(0.04),
+                  blurRadius: 80,
+                  spreadRadius: 80,
+                  offset: Offset(0, 0),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
